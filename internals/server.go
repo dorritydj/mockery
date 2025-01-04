76edef9616e5e7bc971536995ac17e2264640754
dorritydj <dorritydj@gmail.com>
2025-01-03 23:36:37 -0600

    refactor: moving route generation to internals. scaffolding server.

---
+++ internals/server.go
@@ -0,0 +1,50 @@
+package internals
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"net/http"
+	"os"
+)
+
+type Server struct {
+	Mux *http.ServeMux
+}
+
+func (s *Server) ParseFile(path string, d fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+
+	if !d.IsDir() {
+		var endpoint Endpoint
+
+		file, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		err = json.Unmarshal(file, &endpoint)
+		if err != nil {
+			return err
+		}
+
+		req := fmt.Sprintf("%s %s", endpoint.Method, endpoint.ParseUri())
+		def := endpoint.GetDefaultResponse()
+
+		fmt.Println(req)
+
+		s.Mux.HandleFunc(req, func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(def.Status)
+
+			data, err := json.Marshal(def.Body)
+			if err != nil {
+				fmt.Println("error converting body to json")
+			}
+
+			w.Write(data)
+		})
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/fs"
 	"net/http"
-	"os"
 	"path/filepath"
 
 	"mockery.dorrity.dj/internals"
@@ -14,55 +11,21 @@ import (
 func main() {
 	dir := "./routes"
 
-	mux := http.NewServeMux()
+	app := &internals.Server{
+		Mux: http.NewServeMux(),
+	}
 
-	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if !d.IsDir() {
-			var endpoint internals.Endpoint
-
-			file, err := os.ReadFile(path)
-			if err != nil {
-				return err
-			}
-
-			err = json.Unmarshal(file, &endpoint)
-			if err != nil {
-				return err
-			}
-
-			// fmt.Printf("%#v\n", endpoint)
-
-			req := fmt.Sprintf("%s %s", endpoint.Method, endpoint.ParseUri())
-			fmt.Println(req)
-			def := endpoint.GetDefaultResponse()
-
-			mux.HandleFunc(req, func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(def.Status)
-
-				data, err := json.Marshal(def.Body)
-				if err != nil {
-					fmt.Println("error converting body to json")
-				}
-
-				w.Write(data)
-			})
-		}
-		return nil
-	})
+	err := filepath.WalkDir(dir, app.ParseFile)
 
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 
-	mux.HandleFunc("GET /{slug}", func(w http.ResponseWriter, r *http.Request) {
+	app.Mux.HandleFunc("GET /{slug}", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r)
 	})
 
-	err = http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", app.Mux)
 	if err != nil {
 		fmt.Println("error starting server")
 	}
